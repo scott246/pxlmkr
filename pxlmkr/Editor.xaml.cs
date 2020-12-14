2004dff792e52b355cda7dbcb46eb62a11cbb868
scott246 <ndscott32@gmail.com>
2020-12-13 19:19:23 -0500

    editor panel improvements

--- pxlmkr/Editor.xaml.cs
+++ pxlmkr/Editor.xaml.cs
@@ -18,9 +18,9 @@ namespace pxlmkr
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class Editor : Window
 	{
-		public MainWindow()
+		public Editor()
 		{
 			InitializeComponent();
 		}
