ac31571b8ecd02240f0553115dad1487a4b9a204
scott246 <ndscott32@gmail.com>
2020-12-19 14:05:41 -0500

    improved grid lines

--- pxlmkr/Editor.xaml.cs
+++ pxlmkr/Editor.xaml.cs
@@ -20,78 +20,56 @@ namespace pxlmkr
 	/// </summary>
 	public partial class Editor : Window
 	{
-        UIElement gridLines;
 		public Editor()
 		{
 			InitializeComponent();
 		}
 
-        private void ClearCanvasGridLines()
+        private void InitializeCanvasGridLines()
 		{
-            PixelCanvas.Children.Remove(gridLines);
-        }
-
-		private void InitializeCanvasGridLines()
-		{
-            Image lines = new Image();
-
-            DrawingVisual gridLinesVisual = new DrawingVisual();
-            DrawingContext dct = gridLinesVisual.RenderOpen();
-            Brush lightPenBrush = new SolidColorBrush(Colors.LightGray);
-            Pen lightPen = new Pen(lightPenBrush, 0.5);
-            lightPen.Freeze();
-
-            int rows = 15;
-            int columns = 20;
-
+            int rows = 16;
+            int cols = 16;
             double canvasHeight = PixelCanvas.ActualHeight;
             double canvasWidth = PixelCanvas.ActualWidth;
-
-            // Horizontal lines
-            Point left = new Point(0, 0);
-            Point right = new Point(canvasWidth, 0);
             double rowHeight = canvasHeight / rows;
+            double colWidth = canvasWidth / cols;
 
-            for (int i = 0; i <= rows; i++)
-            {
-                dct.DrawLine(lightPen, left, right);
-                left.Offset(0, rowHeight);
-                right.Offset(0, rowHeight);
+            for (int curRow = 0; curRow <= rows; curRow++)
+			{
+                Line gridLine = new Line
+                {
+                    X1 = 0,
+                    X2 = canvasWidth,
+                    Y1 = rowHeight * curRow,
+                    Y2 = rowHeight * curRow,
+                    Stroke = Brushes.DarkGray,
+                    StrokeThickness = 2
+				};
+                PixelCanvas.Children.Add(gridLine);
             }
-
-            // Vertical lines
-            Point top = new Point(0, 0);
-            Point bottom = new Point(0, canvasHeight);
-            double colWidth = canvasWidth / columns;
-
-            for (int i = 0; i <= columns; i++)
+            for (int curCol = 0; curCol <= cols; curCol++)
             {
-                dct.DrawLine(lightPen, top, bottom);
-                top.Offset(colWidth, 0);
-                bottom.Offset(colWidth, 0);
+                Line gridLine = new Line
+                {
+                    X1 = colWidth * curCol,
+                    X2 = colWidth * curCol,
+                    Y1 = 0,
+                    Y2 = canvasHeight,
+                    Stroke = Brushes.DarkGray,
+                    StrokeThickness = 2
+                };
+                PixelCanvas.Children.Add(gridLine);
             }
-
-            dct.Close();
-
-            // Render
-            RenderTargetBitmap bmp = new RenderTargetBitmap((int)canvasWidth, (int)canvasHeight, 96, 96, PixelFormats.Pbgra32);
-            bmp.Render(gridLinesVisual);
-            bmp.Freeze();
-            lines.Source = bmp;
-            gridLines = lines;
-            PixelCanvas.Children.Add(lines);
-        }
+		}
 
 		private void PixelCanvas_Loaded(object sender, RoutedEventArgs e)
 		{
-            ClearCanvasGridLines();
             InitializeCanvasGridLines();
         }
 
-		private void PixelCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+		private void PixelCanvas_MouseDown(object sender, MouseButtonEventArgs e)
 		{
-            ClearCanvasGridLines();
-            InitializeCanvasGridLines();
+
 		}
 	}
 }
