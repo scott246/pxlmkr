61102b23366ff624911917e87d2f50fbf8e2dc42
scott246 <ndscott32@gmail.com>
2020-12-20 21:47:14 -0500

    create help text

--- pxlmkr/Editor.xaml.cs
+++ pxlmkr/Editor.xaml.cs
@@ -76,28 +76,103 @@ namespace pxlmkr
 		private void PixelCanvas_Loaded(object sender, RoutedEventArgs e)
         {
             CalculateGridInfo();
+            UpdateHelpText("");
             InitializeCanvasGridLines();
         }
 
 		private void PixelCanvas_MouseDown(object sender, MouseButtonEventArgs e)
 		{
-
+            // paint
 		}
 
         private void PixelCanvas_MouseMove(object sender, MouseEventArgs e)
         {
             Point mousePos = Mouse.GetPosition(PixelCanvas);
             int rowPos = (int)(mousePos.X / rowHeight) + 1;
-            if (rowPos > rows)
-            {
-                rowPos = rows;
-            }
             int colPos = (int)(mousePos.Y / colWidth) + 1;
-            if (colPos > cols)
-			{
-                colPos = cols;
-			}
+            if (rowPos > rows) rowPos = rows;
+            if (colPos > cols) colPos = cols;
             CursorPositionLabel.Content = rowPos + " : " + colPos;
+		}
+
+		private void ProjectSizeLabel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+            // allow project size to be changed
+		}
+
+		private void PixelSizeLabel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+            // allow pixel size to be changed
+		}
+
+        public void UpdateHelpText(string newHelpText)
+		{
+            HelpTextLabel.Content = newHelpText;
+		}
+
+		private void PixelSizeLabel_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Project pixel size (in screen pixels), click to change");
+		}
+
+		private void ProjectSizeLabel_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Project dimensions, click to change");
+		}
+
+        private void ClearHelpText(object sender, MouseEventArgs e)
+        {
+            UpdateHelpText("");
+        }
+
+		private void CursorPositionLabel_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Position (x : y)");
+		}
+
+		private void PaintToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Paint tool");
+		}
+
+		private void SelectToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Select tool");
+		}
+
+		private void EraserToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Eraser tool");
+		}
+
+		private void FillToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Fill tool");
+        }
+
+		private void EyedropperToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Eyedropper tool");
+		}
+
+		private void LineToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Line tool");
+		}
+
+		private void ShapeToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Shape tool");
+		}
+
+		private void TextToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Text tool");
+		}
+
+		private void CropToolButton_MouseEnter(object sender, MouseEventArgs e)
+		{
+            UpdateHelpText("Crop tool");
 		}
 	}
 }
